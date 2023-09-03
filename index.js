fff3e21b85961376bcd7a8c7b2ee8e482bd7d740
enginorun <65984850+enginorun@users.noreply.github.com>
2023-09-04 01:11:58 +0300

    entafix-xml

--- index.js
+++ index.js
@@ -5,28 +5,32 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
  res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
  res.write('Merhaba Dünya! TEST');
-
  res.end();
 });
 
+function makeRequest() {
+  var config = {
+    method: 'GET',
+    url: 'https://8ab3-95-70-128-52.ngrok-free.app/xmlapp/includes/php/bring-products.php'
+  };
+
+  axios(config)
+    .then(function(response) {
+      console.log("\n" + response.data + "\n");
+      // İstek tamamlandıktan sonra tekrar isteği başlatmak için makeRequest'i çağırıyoruz
+      setTimeout(makeRequest, 3000);
+    })
+    .catch(function(error) {
+      console.log("HATA -BAŞLANGIÇ");
+      console.log(error.response);
+      console.log("HATA -BİTİŞ");
+      // Hata durumunda da tekrar isteği başlatmak için makeRequest'i çağırıyoruz
+      makeRequest();
+    });
+}
 
 server.listen(PORT, () => {
  console.log('Uygulama çalıştırıldı.');
-
-var config = {
- method: 'GET',
- url: 'https://0797-95-70-128-223.ngrok-free.app/entafix/includes/php/queue-code-generation.php'
-};
- 
-setInterval(function(){
-  axios(config)
-  .then(function(response) {
-    console.log("\n"+response.data+"\n");
-  })
-  .catch(function (error) {
-    console.log("HATA -BAŞLANGIÇ");
-    console.log(error.response);
-    console.log("HATA -BİTİŞ");
-  });
-},10000);
+ // İlk isteği başlatıyoruz
+ makeRequest();
 });
