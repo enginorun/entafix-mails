484756d818b49c57344de6d3317806e1feb157b3
enginorun <65984850+enginorun@users.noreply.github.com>
2023-09-06 17:49:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
 function makeRequest() {
   var config = {
     method: 'GET',
-    url: 'https://6026-95-70-128-52.ngrok-free.app/xmlapp/includes/php/bring-products.php'
+    url: https://1e22-95-70-128-52.ngrok-free.app/xmlapp/includes/php/bring-products.php'
   };
 
   axios(config)
