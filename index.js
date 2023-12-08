3f115ad13e025f4224f2761f755721e726a07916
enginorun <65984850+enginorun@users.noreply.github.com>
2023-12-08 15:45:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
 function makeRequest() {
   var config = {
     method: 'GET',
-    url: 'https://fb2a-95-70-128-173.ngrok-free.app/shopify-apps/bpe/includes/php/price-editor.php'
+    url: 'https://1efb-95-70-128-173.ngrok-free.app/shopify-apps/bpe/includes/php/price-editor.php'
   };
 
   axios(config)
